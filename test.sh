67b7933887d5fa59f189f6a6045104c139744de9
Salemmous <thomas.debluts@gmail.com>
2019-01-31 11:48:15 +0200

    Better test

--- test.sh
+++ test.sh
@@ -1,20 +1,38 @@
-#!/bin/bash -l
-#SBATCH -J onmt_setup
-#SBATCH -o out_%J.onmt_setup.txt
-#SBATCH -e err_%J.onmt_setup.txt
+#!/bin/bash
+# created: Jan 31, 2019 11:43 AM
+# author: deblutst
+#SBATCH -J TestPreprocess
+#SBATCH -o preprocessTestOutput
+#SBATCH -e preprocessTestError
 #SBATCH -p gpu
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -t 01:00:00
-#SBATCH --mem-per-cpu=1g
-#SBATCH --mail-type=NONE
-#SBATCH --gres=gpu:p100:1
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:K80:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=thomas.debluts@gmail.com
+
+# note, this job requests a total of 1 cores and 1 GPGPU cards
+# note, submit the job from taito-gpu.csc.fi
+# commands to manage the batch script
+#   submission command
+#     sbatch [script-file]
+#   status command
+#     squeue -u deblutst
+#   termination command
+#     scancel [jobid]
+
+# For more information
+#   man sbatch
+#   more examples in Taito GPU guide in
+#   http://research.csc.fi/taito-gpu
 
 # run commands
 module purge
 # you will need to load these modules every time you use the neural-intelingua branch 
 module load python-env/intelpython3.6-2018.3 gcc/5.4.0 cuda/9.0 cudnn/7.1-cuda9
 
+ONMT=/homeappl/home/deblutst/OpenNMT-py
 SAVE_PATH=$ONMT/models/demo
 mkdir -p $SAVE_PATH
 python train.py -data data/sample_data/de-cs/data \
@@ -46,3 +64,9 @@ python train.py -data data/sample_data/de-cs/data \
              -batch_size 256 \
              -gpuid 0 \
              -save_checkpoint_steps 10000
+
+# This script will print some usage statistics to the
+# end of file: preprocessTestOutput
+# Use that to improve your resource request estimate
+# on later jobs.
+seff $SLURM_JOBID
