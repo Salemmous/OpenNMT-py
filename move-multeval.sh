158bf66a9194e84b51a92a97058633a61844e8c6
Salemmous <thomas.debluts@gmail.com>
2019-05-13 22:58:37 +0300

    Fixes

--- move-multeval.sh
+++ move-multeval.sh
@@ -2,22 +2,34 @@
 ONMT=/homeappl/home/deblutst/OpenNMT-py
 MULT=/homeappl/home/deblutst/multeval-0.5.1
 SAVE_PATH=$ONMT/models/demo
-DATADIR=$ONMT/data
+DATA_DIR=$ONMT/data
+
+mkdir -p $MULT/predictions
+mkdir -p $MULT/predictions/multilingual
+mkdir -p $MULT/predictions/mono
+mkdir -p $MULT/train-set
+
 for src_lang in en "fi" fr_be nl sv
 do
 	for bpe in default bpe-10 bpe-100
-	cp $SAVE_PATH/$bpe/MULTILINGUAL_prediction_${src_lang}.txt $MULT/predictions/multilingual/$bpe/$src_lang.txt
+	do
+		mkdir -p $MULT/predictions/multilingual/$bpe
+		cp $SAVE_PATH/$bpe/MULTILINGUAL_prediction_${src_lang}.txt $MULT/predictions/multilingual/$bpe/$src_lang.txt
 	done
 done
 for src_lang in "fi" nl
 do
         for bpe in default bpe-10 bpe-100
-        cp $SAVE_PATH/mono/$bpe/MULTILINGUAL_prediction_${src_lang}.txt $MULT/predictions/mono/$bpe/$src_lang.txt
+	do
+                mkdir -p $MULT/predictions/mono/$bpe
+        	cp $SAVE_PATH/mono/$bpe/MULTILINGUAL_prediction_${src_lang}.txt $MULT/predictions/mono/$bpe/$src_lang.txt
         done
 done
 for src_lang in en "fi" fr_be nl sv
 do
         for bpe in sign sign-bpe-10 sign-bpe-100
-        cp $DATA_DIR/$bpe/${src_lang}/train.sign $MULT/train-set/$bpe/$src_lang.txt
+	do
+                mkdir -p $MULT/train-set/$bpe
+        	cp $DATA_DIR/$bpe/${src_lang}/train.sign $MULT/train-set/$bpe/$src_lang.txt
         done
 done
